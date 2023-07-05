4a580ab5c0c36f4a657b086ef093ca7ddf5a4292
racheltamayo <20-00631@g.batstate-u.edu.ph>
2023-07-05 11:27:00 +0800

    rachel

--- ecoexchange/src/app/home/home.page.ts
+++ ecoexchange/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  logoUrl: SafeResourceUrl;
+  isMinimized = false;
+  showHeaderContent = true;
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {
+    this.logoUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/logo.png'); // for the recyclable logo
+    this.checkScreenSize();
+  }
 
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.checkScreenSize();
+  }
+  private checkScreenSize() { // for showing the menu
+    this.isMinimized = window.innerWidth <= 700;
+    this.showHeaderContent = window.innerWidth > 700;
+  }
 }
